72bfe9f73ea55752281bfda7f5df2532f43c2081
riveroarmando <riveroarmando@gmail.com>
2024-12-20 17:38:07 -0300

    Menu funcionando completo con logout

--- src/app/home/menu/menu.component.ts
+++ src/app/home/menu/menu.component.ts
@@ -1,6 +1,6 @@
 import { Component, computed, Input, signal } from '@angular/core';
 import { CommonModule } from '@angular/common'
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 /* Angular Material */
 import { MatListModule } from '@angular/material/list';
@@ -8,6 +8,9 @@ import { MatIconModule } from '@angular/material/icon'
 
 /* Componente customizado */
 import { MenuItemComponent } from "../menu-item/menu-item.component";
+import { SecurityService } from '../../services/security.services';
+import { User } from '../../models/user';
+import { Global } from '../../services/global';
 
 /* Element of menu */
 export type MenuItem = {
@@ -26,11 +29,22 @@ export type MenuItem = {
 })
 export class MenuComponent {
 
+  public usuario!: User;
+    public url: string;
+    
   sideNavCollapsed=signal(false);
   @Input() set collapsed(val: boolean){
     this.sideNavCollapsed.set(val);
   }
 
+  constructor(
+      private _securitydService: SecurityService,
+      private _router: Router
+      ){
+      this.usuario=this._securitydService.usuario;
+      this.url=Global.url;
+    }
+    
   menuItems = signal<MenuItem[]>([
     {
       icon: 'list_alt',
@@ -79,4 +93,9 @@ export class MenuComponent {
   ]);
 
   profilePicSize= computed(()=> this.sideNavCollapsed() ? '32' : '100');
+
+  logout(){
+    this._securitydService.logout();
+    this._router.navigateByUrl("/");
+  }
 }
